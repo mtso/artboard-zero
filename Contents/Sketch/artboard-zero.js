48a94351b1b4cf242e914ea40762fad4fe3508ce
mtso <mtso@mail.com>
2016-08-18 14:00:56 -0700

    Add zoom to fit artboard

--- Contents/Sketch/artboard-zero.js
+++ Contents/Sketch/artboard-zero.js
@@ -3,33 +3,34 @@
 var onArtboardChanged = function(context) {
 
 	action = context.actionContext;
-
 	document = action.document;
-	
-	page = document.currentPage();
 
+	page = document.currentPage();
 	artboards = page.artboards();
 
 	count = artboards.count();
-
 	if (count == 1) {
-		document.showMessage("Moved the first artboard to (0, 0).");
 
+		// Get the first artboard
 		firstArtboard = artboards[0];
 
-		frame = firstArtboard.frame();
-		
+		// Move the artboard's frame to the origin
+		frame = firstArtboard.frame();		
 		frame.x = 0;
 		frame.y = 0;
 
+		document.showMessage("Moved the first artboard to (0, 0).");
 
-		view = document.currentView();
-		rectToCenter = page.rect();
-		view.centerRect(rectToCenter);
-
+		// Deselect all layers
+		// Then reselect the first artboard to reload the inspector
+		// This is a workaround because MSDocument.reloadInspector(:) doesn't seem to work
 		page.deselectAllLayers();
-
 		firstArtboard.select_byExpandingSelection(true, true);
+
+		// Center the view on the artboard
+		view = document.currentView();
+		offsetRect = firstArtboard.rect().scaleBy(1.2);
+		view.zoomToFitRect(offsetRect);
 	}
 
 };
--- Contents/Sketch/reload-inspector.js
+++
@@ -1,6 +0,0 @@
-
-var reloadInspector = function(context) {
-	document = context.document;
-	document.reloadInspector();
-	document.showMessage("Reloading inspector.");
-}
